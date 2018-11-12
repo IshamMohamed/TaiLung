5af19a6042a2e49e03146de359fa3bff166619a3
Isham <isham@kloudynet.com>
2018-11-12 19:10:38 +0800

    Implemented States and the MainBotAccessors

--- TaiLung/MainBotAccessors.cs
+++ TaiLung/MainBotAccessors.cs
@@ -1,6 +1,21 @@
-﻿namespace TaiLung
+﻿using Microsoft.Bot.Builder;
+using System;
+using System.Threading.Tasks;
+using TaiLung.States;
+
+namespace TaiLung
 {
-    internal class MainBotAccessors
+    public class MainBotAccessors
     {
+        private BotState generalBotState { get; }
+        public IStatePropertyAccessor<MainState> mainState { get; }
+
+        public MainBotAccessors(BotState botState)
+        {
+            generalBotState = botState ?? throw new ArgumentNullException(nameof(botState));
+            mainState = generalBotState.CreateProperty<MainState>($"{nameof(MainBotAccessors)}.{nameof(MainState)}");
+        }
+
+        public Task SaveChangesAsync(ITurnContext turnContext) => generalBotState.SaveChangesAsync(turnContext);
     }
 }
---
+++ TaiLung/States/MainState.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaiLung.States
+{
+    public class MainState
+    {
+        public string GeneralProperty { get; set; }
+    }
+}
