9247538f94e58b75109fb69473ba1e63e9652c9d
Isham <isham@kloudynet.com>
2018-11-12 22:41:48 +0800

    Added DialogState

--- TaiLung/MainBotAccessors.cs
+++ TaiLung/MainBotAccessors.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
 using System;
 using System.Threading.Tasks;
 using TaiLung.States;
@@ -9,11 +10,13 @@ namespace TaiLung
     {
         private BotState generalBotState { get; }
         public IStatePropertyAccessor<MainState> mainState { get; }
+        public IStatePropertyAccessor<DialogState> dialogState { get; } //DialogState comes from using Microsoft.Bot.Builder.Dialogs;
 
         public MainBotAccessors(BotState botState)
         {
             generalBotState = botState ?? throw new ArgumentNullException(nameof(botState));
             mainState = generalBotState.CreateProperty<MainState>($"{nameof(MainBotAccessors)}.{nameof(MainState)}");
+            dialogState = generalBotState.CreateProperty<DialogState>($"{nameof(MainBotAccessors)}.{nameof(DialogState)}");
         }
 
         public Task SaveChangesAsync(ITurnContext turnContext) => generalBotState.SaveChangesAsync(turnContext);
